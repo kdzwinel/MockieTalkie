e9b710fe047c23382654aca7f31c0392b22d7fab
Konrad Dzwinel <kdzwinel@gmail.com>
2014-06-29 03:32:50 +0200

    Improving looks of the options page.

--- options_page/app/components/mock-form/mock-form.js
+++ options_page/app/components/mock-form/mock-form.js
@@ -2,6 +2,7 @@
 
 angular.module('optionsPage.components')
   .controller('mockFormComponentCtrl', function ($scope, $element) {
+    $scope.methodsHTTP = ['GET', 'PUT', 'DELETE', 'POST', 'HEAD', 'TRACE', 'CONNECT'];
   })
   .component('mockForm', function () {
     return {
