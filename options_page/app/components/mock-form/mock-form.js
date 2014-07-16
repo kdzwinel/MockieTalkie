d1e9f430aacd551ad957c34e5bc4e8d8056ad774
Konrad Dzwinel <kdzwinel@gmail.com>
2014-07-17 00:09:28 +0200

    Moving HTTP codes and methods to config.

--- options_page/app/components/mock-form/mock-form.js
+++ options_page/app/components/mock-form/mock-form.js
@@ -1,50 +1,9 @@
 'use strict';
 
 angular.module('optionsPage.components')
-  .controller('mockFormComponentCtrl', function ($scope, $element) {
-    $scope.methodsHTTP = ['GET', 'PUT', 'DELETE', 'POST', 'HEAD', 'TRACE', 'CONNECT'];
-    $scope.responseCodesHTTP = [
-      {value: 200, text: 'OK'},
-      {value: 201, text: 'Created'},
-      {value: 202, text: 'Accepted'},
-      {value: 203, text: 'Non-Authoritative Information'},
-      {value: 204, text: 'No Content'},
-      {value: 205, text: 'Reset Content'},
-      {value: 206, text: 'Partial Content'},
-      {value: 300, text: 'Multiple Choices'},
-      {value: 301, text: 'Moved Permanently'},
-      {value: 302, text: 'Found'},
-      {value: 303, text: 'See Other'},
-      {value: 304, text: 'Not Modified'},
-      {value: 306, text: 'Switch Proxy'},
-      {value: 307, text: 'Temporary Redirect'},
-      {value: 308, text: 'Resume Incomplete'},
-      {value: 400, text: 'Bad Request'},
-      {value: 401, text: 'Unauthorized'},
-      {value: 402, text: 'Payment Required'},
-      {value: 403, text: 'Forbidden'},
-      {value: 404, text: 'Not Found'},
-      {value: 405, text: 'Method Not Allowed'},
-      {value: 406, text: 'Not Acceptable'},
-      {value: 407, text: 'Proxy Authentication Required'},
-      {value: 408, text: 'Request Timeout'},
-      {value: 409, text: 'Conflict'},
-      {value: 410, text: 'Gone'},
-      {value: 411, text: 'Length Required'},
-      {value: 412, text: 'Precondition Failed'},
-      {value: 413, text: 'Request Entity Too Large'},
-      {value: 414, text: 'Request-URI Too Long'},
-      {value: 415, text: 'Unsupported Media Type'},
-      {value: 416, text: 'Requested Range Not Satisfiable'},
-      {value: 417, text: 'Expectation Failed'},
-      {value: 500, text: 'Internal Server Error'},
-      {value: 501, text: 'Not Implemented'},
-      {value: 502, text: 'Bad Gateway'},
-      {value: 503, text: 'Service Unavailable'},
-      {value: 504, text: 'Gateway Timeout'},
-      {value: 505, text: 'HTTP Version Not Supported'},
-      {value: 511, text: 'Network Authentication Required'}
-    ];
+  .controller('mockFormComponentCtrl', function ($scope, $element, HTTPmethods, HTTPcodes) {
+    $scope.methodsHTTP = HTTPmethods;
+    $scope.responseCodesHTTP = HTTPcodes;
 
     $scope.refreshEditor = function() {
       $scope.refreshEditorHelper = !$scope.refreshEditorHelper;
--- options_page/app/config/config.js
+++ options_page/app/config/config.js
@@ -12,4 +12,47 @@ angular.module('optionsPage')
       return 'components/' + componentSnakeName + '/' + componentSnakeName + '.html';
     })
   })
-  .value('cgBusyTemplateName', 'views/angular-busy/default-spinner.html');
+  .value('cgBusyTemplateName', 'views/angular-busy/default-spinner.html')
+  .value('HTTPmethods', ['GET', 'PUT', 'DELETE', 'POST', 'HEAD', 'TRACE', 'CONNECT'])
+  .value('HTTPcodes', [
+    {value: 200, text: 'OK'},
+    {value: 201, text: 'Created'},
+    {value: 202, text: 'Accepted'},
+    {value: 203, text: 'Non-Authoritative Information'},
+    {value: 204, text: 'No Content'},
+    {value: 205, text: 'Reset Content'},
+    {value: 206, text: 'Partial Content'},
+    {value: 300, text: 'Multiple Choices'},
+    {value: 301, text: 'Moved Permanently'},
+    {value: 302, text: 'Found'},
+    {value: 303, text: 'See Other'},
+    {value: 304, text: 'Not Modified'},
+    {value: 306, text: 'Switch Proxy'},
+    {value: 307, text: 'Temporary Redirect'},
+    {value: 308, text: 'Resume Incomplete'},
+    {value: 400, text: 'Bad Request'},
+    {value: 401, text: 'Unauthorized'},
+    {value: 402, text: 'Payment Required'},
+    {value: 403, text: 'Forbidden'},
+    {value: 404, text: 'Not Found'},
+    {value: 405, text: 'Method Not Allowed'},
+    {value: 406, text: 'Not Acceptable'},
+    {value: 407, text: 'Proxy Authentication Required'},
+    {value: 408, text: 'Request Timeout'},
+    {value: 409, text: 'Conflict'},
+    {value: 410, text: 'Gone'},
+    {value: 411, text: 'Length Required'},
+    {value: 412, text: 'Precondition Failed'},
+    {value: 413, text: 'Request Entity Too Large'},
+    {value: 414, text: 'Request-URI Too Long'},
+    {value: 415, text: 'Unsupported Media Type'},
+    {value: 416, text: 'Requested Range Not Satisfiable'},
+    {value: 417, text: 'Expectation Failed'},
+    {value: 500, text: 'Internal Server Error'},
+    {value: 501, text: 'Not Implemented'},
+    {value: 502, text: 'Bad Gateway'},
+    {value: 503, text: 'Service Unavailable'},
+    {value: 504, text: 'Gateway Timeout'},
+    {value: 505, text: 'HTTP Version Not Supported'},
+    {value: 511, text: 'Network Authentication Required'}
+  ]);
